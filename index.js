cba930c6d666294bed9b19c56db3cfc6b784402e
hhdg1515 <144937930+hhdg1515@users.noreply.github.com>
2024-10-08 23:28:47 -0700

    Update index.js

--- index.js
+++ index.js
@@ -9,21 +9,21 @@ const literToGallon =  0.264
 const kiloToPound =  2.204
 
 convertBtn.addEventListener("click", function () {
-    let baseValue =  inputEl.value;
+    let baseValue =  Number(inputEl.value);
     let getFeet= baseValue*meterToFeet;
     let getMeters= baseValue/meterToFeet;
     lengthEl.textContent = `${baseValue} meters = ${getFeet.toFixed(3)} feet | ${baseValue} feet = ${getMeters.toFixed(3)} meters`
 })
 
 convertBtn.addEventListener("click", function() {
-    let baseValue = inputEl.value;
+    let baseValue = Number(inputEl.value);
     let getGallons= baseValue*literToGallon;
     let getLitters= baseValue/literToGallon;
     volumeEl.textContent = `${baseValue} liters = ${getGallons.toFixed(3)} gallons | ${baseValue} gallons = ${getLitters.toFixed(3)} liters` 
 })
 
 convertBtn.addEventListener("click", function() {
-    let baseValue = inputEl.value
+    let baseValue = Number(inputEl.value);
     let getPounds= baseValue*kiloToPound;
     let getKilos= baseValue/kiloToPound;
     weightEl.textContent = `${baseValue} kilos = ${getPounds.toFixed(3)} pounds| ${baseValue} pounds = ${getKilos.toFixed(3)} kilos` 
